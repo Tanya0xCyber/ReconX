6a1a63a63e45b5c691d78e62ca5095f8274311b5
TANYA SINGH <tanyarajput7505@gmail.com>
2026-06-20 22:36:48 +0530

    Update reconx.py

--- reconx.py
+++ reconx.py
@@ -1492,7 +1492,7 @@ def main():
 # NOW print everything in the RIGHT order:
 # Executive summary first, raw data at bottom
     if not config["quiet"]:
-        print_executive_summary(results, config,
+        print_intelligence_report(results, config,
                             time.time() - total_start)
         print_passive_results(results)
         print_active_results(results)
